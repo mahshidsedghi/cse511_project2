b8f3dccd2e035bdb75bb0c61fa56c11ede97efe8
Narges <narges.shahidi@yahoo.com>
2013-11-30 17:50:02 -0500

    StringFunction

--- StringFunctions.hh
+++ StringFunctions.hh
@@ -2,7 +2,24 @@
 #include <sstream>
 using namespace std; 
 
-string nextToken(string &str); 
-string toLower(string str); 
+string nextToken(string &str){
+	string token; 
+	istringstream iss(str); 
+	iss >> token; 
+	str = "";  
+	while (iss){
+		string temp; 
+		iss >> temp; 
+		str += temp + " "; 
+	}
+	return token; 
+} 
+string toLower(string str){
+	string lower (""); 
+	for (unsigned int i=0; i < str.length(); ++i)
+		lower += tolower(str[i]); 
+	
+	return lower; 
+} 
 
 
