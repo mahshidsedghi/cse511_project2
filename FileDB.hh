b195804f2006cd00827321fe4d50ec85a7c841d7
narges64 <nxs314@mamberamo.cse.psu.edu>
2013-12-06 21:57:47 -0500

    saho

---
+++ FileDB.hh
@@ -0,0 +1,5 @@
+
+struct FileDB{
+	string file_name;
+	fileRecipe file_recipe;
+};
---
+++ FileServer.hh
@@ -0,0 +1,9 @@
+#include "net_addresses.hh"
+
+void execFunc_create ( TCPSocket * sock, string arguments ); 
+void execFunc_read   ( TCPSocket * sock, string arguments ); 
+void execFunc_write  ( TCPSocket * sock, string arguments ); 
+void execFunc_delete ( TCPSocket * sock, string arguments ); 
+
+
+
